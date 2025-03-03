b1a44134d0b1f907541d64ac7acd8fd6e3012153
kartavya7131 <kartavya830@gmail.com>
2025-03-03 12:21:22 -0800

    organization
    create folder for scene and scripts

--- script/game_scene_1.gd
+++ script/game_scene_1.gd
@@ -12,4 +12,5 @@ func _process(delta: float) -> void:
 
 
 func _on_home_pressed():
-	get_tree().change_scene_to_file("res://MainMenu.tscn")
+	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn"
+	)
--- script/main_menu.gd
+++ script/main_menu.gd
@@ -13,7 +13,7 @@ func _process(delta: float) -> void:
 
 
 func _on_Start_pressed():
-	get_tree().change_scene_to_file("res://GameScene1.tscn")
+	get_tree().change_scene_to_file("res://scenes/GameScene1.tscn")
 
 
 func _on_info_button_pressed():
