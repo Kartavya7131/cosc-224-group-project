8c61b9a89456185189965316d0ed4a2541fb89f2
kartavya7131 <kartavya830@gmail.com>
2025-03-06 10:34:52 -0800

    updated to 4.4
    plus made the level work better and added hint

--- script/attackerLevel1.gd
+++ script/attackerLevel1.gd
@@ -11,18 +11,30 @@ func _process(delta: float) -> void:
 	pass
 
 # The correct SQL injection for this level
-var correct_injection = "OR '1' = '1';--"
+var correct_injection = "OR'1'='1';"
+
+# Attempt tracking
+var attempt_count = 0
+
+
+func _on_goBack_pressed():
+	get_tree().change_scene_to_file("res://scenes/AttackerPage.tscn")
 
 # UI Elements
 @onready var input_field = $LineEdit
 @onready var feedback_label = $FeedbackLabel
+@onready var hint_label = $hint_label  # New hint label
 
 # When submit is pressed, validate the input
 func _on_SubmitButton_pressed():
-	var user_input = input_field.text.strip_edges()  # Get the input from LineEdit
+	var user_input = input_field.text.replace(" ", "")  # Get the input from LineEdit
 	if user_input == correct_injection:
 		feedback_label.text = "Injection successful! You've hacked into the system."
 		# Transition to next level
-		#get_tree().change_scene("res://AttackerLevel2.tscn")  # Next level (you'll need to create this next)
+		#get_tree().change_scene("res://AttackerLevel2.tscn")  # Next level (you'll need to create this next
 	else:
-		feedback_label.text = "Incorrect SQL injection. Try again!"
+		attempt_count += 1
+		feedback_label.text = " Incorrect SQL injection. Attempts: " + str(attempt_count)
+		if attempt_count >= 3:
+			hint_label.visible = true   # Show hint after 3 failed attempts
+			hint_label.text = " Hint: Try using 'OR' to make the condition always TRUE."
