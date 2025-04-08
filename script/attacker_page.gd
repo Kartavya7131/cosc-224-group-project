ec82f63475eb0d29390c334141f9d0b182be64e3
KIverson11 <kiverson11@gmail.com>
2025-04-07 19:59:13 -0700

    pushing changes to level 3
    Ui changes and level 3 switched to buttons

--- script/attacker_page.gd
+++ script/attacker_page.gd
@@ -18,9 +18,9 @@ func _on_goBack_pressed():
 func _on_level_1_pressed():
 	get_tree().change_scene_to_file("res://scenes/AttackerLevel1_Modified.tscn")
 func _on_level_2_pressed():
-	get_tree().change_scene_to_file("res://scenes/AttackerLevel2.tscn")
+	get_tree().change_scene_to_file("res://scenes/AttackerLevel2_Modified.tscn")
 func _on_level_3_pressed():
-	get_tree().change_scene_to_file("res://scenes/AttackerLevel3.tscn")
+	get_tree().change_scene_to_file("res://scenes/AttackerLevel3_Modified.tscn")
 func _on_level_4_pressed():
 	get_tree().change_scene_to_file("res://scenes/AttackerLevel4.tscn")
 func _on_level_5_pressed():
