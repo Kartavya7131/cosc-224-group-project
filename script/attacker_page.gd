8d89e18b9b2de1dfad7f4e66413db91556049c5a
kartavya7131 <kartavya830@gmail.com>
2025-04-08 22:22:11 -0700

    reformating the names and answers

--- script/attacker_page.gd
+++ script/attacker_page.gd
@@ -16,10 +16,10 @@ func _on_goBack_pressed():
 
 
 func _on_level_1_pressed():
-	get_tree().change_scene_to_file("res://scenes/AttackerLevel1_Modified.tscn")
+	get_tree().change_scene_to_file("res://scenes/AttackerLevel1.tscn")
 func _on_level_2_pressed():
-	get_tree().change_scene_to_file("res://scenes/AttackerLevel2_Modified.tscn")
+	get_tree().change_scene_to_file("res://scenes/AttackerLevel2.tscn")
 func _on_level_3_pressed():
-	get_tree().change_scene_to_file("res://scenes/AttackerLevel3_Modified.tscn")
+	get_tree().change_scene_to_file("res://scenes/AttackerLevel3.tscn")
 func _on_level_4_pressed():
 	get_tree().change_scene_to_file("res://scenes/AttackerLevel4.tscn")
