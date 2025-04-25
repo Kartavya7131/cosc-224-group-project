85aa158c4443b60d4c98625acdb38d6975d05420
KIverson11 <kiverson11@gmail.com>
2025-04-25 13:54:29 -0700

    Quick UI change
    Add button

--- script/info_codex.gd
+++ script/info_codex.gd
@@ -3,3 +3,7 @@ extends Node2D
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
+
+
+func _on_back_2_pressed():
+	get_tree().change_scene_to_file("res://scenes/GameScene1.tscn")
